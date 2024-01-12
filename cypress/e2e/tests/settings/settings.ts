8509c5f21c6094aa0ece25f4006471326dc101e1
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-01-12 14:23:34 +0100

    settings

--- cypress/e2e/tests/settings/settings.ts
+++ cypress/e2e/tests/settings/settings.ts
@@ -1,12 +1,16 @@
-import loginPage from "../../pages/loginPage";
+import loginPage from "../../../pages/loginPage";
 
-describe('Onboarding', () => {
+describe('Settings', () => {
     beforeEach(() => {
 
         cy.viewport('iphone-x')
         cy.visit("/login");
         loginPage.login("yanik1999@hotmail.com", "1234")
         loginPage.checkLoginSuccess();
+        cy.visit("/app/settings");
+    });
+    it('personal details', () => {
+
     });
 
 });
